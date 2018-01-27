c2c0d380ccf3f0ef3305eada2ebcd91f538326ec
Shadr1k <Artemkauser@Yandex.ru>
2018-01-27 04:13:10 +0300

    04:13 27.01.2018

--- OOP.py
+++ OOP.py
@@ -1,87 +1,84 @@
 from tkinter.filedialog import *
 import os
+import cv2
 
 
-class WindowOnTheScreen:
+class Window:
+    BIND_KEY = "<Button-1>"  # Button-1 is left click of mouse
+    TEXT_LABEL = "-file doesn't choose-"
+    BG = 'white'  # back ground colour
+    FONT = ('Ubuntu', 15)  # font properties
+
     def __init__(self, master):
-        self.master = master
-        self.first_container = Container(master, 0, 0, "First file")
-        self.second_container = Container(master, 1, 0, "Second file")
-        self.window()
-
-    def window(self):
-        """Output window on the screen"""
-        self.master.title("Compare details")  # title window
-        self.master.configure(bg="white")  # back ground color of window
-        self.button("Compare", 2, 0, lambda event: self.check(event))
-
-    def button(self, text, row_place, column_place, func_bind):
-        """Place button in the window"""
-        button = Button(self.master, text=text, width=10, font=('Ubuntu', 15), bg='white')
-        button.grid(row=row_place, column=column_place, sticky="w")
-        button.bind('<Button-1>', func_bind)  # bind button by func
+        master.title("Compare details")  # title window
+        master.configure(bg="white")  # back ground color of window
+        self.containers = [Container(master, 0, 0, "First file"), Container(master, 1, 0, "Second file")]
+        button = Button(master, text='Compare', width=10, font=self.FONT, bg=self.BG)
+        button.grid(row=2, column=0, sticky="w")
+        button.bind(self.BIND_KEY, self.check)  # bind button by func
+        self.circuits = []
 
     def check(self, event):
-        if self.first_container.state is True and self.second_container.state is True:
-            print("good")
-        else:
-            print("bad")
+        if all([c.state for c in self.containers]):
+            first_circuit, second_circuit = [c.path for c in self.containers]
+            self.circuits = [Handler(first_circuit), Handler(second_circuit)]
+            a, b = [c.circuit for c in self.circuits]
+            print(a, b)
 
 
 class Handler:
-    def compare(self):
-        pass
-
+    def __init__(self, path):
+        img = cv2.imread(path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray_blur = cv2.GaussianBlur(gray, (9, 9), 1)
+        edged = cv2.Canny(gray_blur, 15, 30)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
+        closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+        circuit = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[1]
+        circuit_max = len(circuit[0])
+        circuit_max_id = 0
+        for i in range(len(circuit)):
+            if circuit_max < len(circuit[i]):
+                circuit_max = len(circuit[i])
+                circuit_max_id = i
+        self.circuit = circuit[circuit_max_id]
 
 class Container:
     def __init__(self, master, row, column, text_button):
-        self.master = master
-        self.row_button = row
-        self.column_button = column
-        self.column_label = column + 1
-        self.text_button = text_button
-        self.text_label = "-file doesn't choose-"
-        self.bind_key = "<Button-1>"  # Button-1 is left click of mouse
-        self.bg = 'white'  # back ground colour
-        self.font = ('Ubuntu', 15)  # font properties
-        self.fg_err = 'red'  # colour of error text
         self.state = False  # state of func
-        self.button()
-        self.label(self.text_label)
-
-    def button(self):
-        """Place button in the window"""
-        button = Button(self.master, text=self.text_button, width=10, font=self.font, bg=self.bg)
-        button.grid(row=self.row_button, column=self.column_button, sticky="w")
-        button.bind(self.bind_key, lambda event: self.open_file_event(event))  # bind button by func
-
-    def label(self, text, fg='black'):
-        """Place label in the window"""
-        label = Label(self.master, width=20, text=text, font=self.font, bg=self.bg, fg=fg)
-        label.grid(row=self.row_button, column=self.column_label)
+        button = Button(master, text=text_button, width=10, font=Window.FONT, bg=Window.BG)
+        button.grid(row=row, column=column, sticky="w")
+        button.bind(Window.BIND_KEY, self.open_file_event)  # bind button by func
+        self.label = Label(master, width=20, text=Window.TEXT_LABEL, font=Window.FONT, bg=Window.BG, fg='black')
+        self.label.grid(row=row, column=column+1)
+        self.path = ""
 
     def open_file_event(self, event):
         """Open window for get path on the file, filter .jpg and .JPG files, filter all errors"""
-        path = askopenfilename()
+        self.path = askopenfilename()
         try:
-            if not path:
-                self.label(self.text_label, self.fg_err)
+            if not self.path:
+                self.label['text'] = Window.TEXT_LABEL
+                self.label['fg'] = 'red'
                 self.state = False
             else:
-                if path.split('.')[-1] == 'jpg' or path.split('.')[-1] == 'JPG':
-                    self.label(os.path.split(path)[-1], 'green')
+                if self.path.split('.')[-1].lower() in ['jpg', 'jpeg']:
+                    self.label['text'] = os.path.split(self.path)[-1]
+                    self.label['fg'] = 'green'
                     # it'll return name of file into label
                     self.state = True
                 else:
-                    self.label("-ErrTypeOfFile-", self.fg_err)
+                    self.label['text'] = "-ErrTypeOfFile-"
+                    self.label['fg'] = 'red'
                     # if choose file which has different enhancing type, it'll return error into label
                     self.state = False
         except TypeError or FileNotFoundError:
-            self.label(self.text_label, self.fg_err)
+            self.label['text'] = Window.TEXT_LABEL
+            self.label['fg'] = 'red'
             self.state = False
             # if return some errors, it'll be painted label in red colour
 
 
 root = Tk()
-gui = WindowOnTheScreen(root)
+gui = Window(root)
 root.mainloop()
